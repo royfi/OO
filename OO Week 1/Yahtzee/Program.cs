52ddfb4b7e0eba2f5ecd46c00d1939df8aa998ed
royfi <royfillekes@hotmail.com>
2016-11-17 10:50:28 +0100

    Update week 1 (Yahtzee)

---
+++ OO Week 1/Yahtzee/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yahtzee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // maak YahtzeeGame (struct) aan
+            YahtzeeGame yahtzeeGame = new YahtzeeGame();
+            // probeer yahtzee te gooien
+            int aantalPogingen = 0;
+            do
+            {
+                // gooi alle dobbelstenen
+                yahtzeeGame.Gooi();
+                aantalPogingen++;
+            } while (!yahtzeeGame.Yahtzee());
+            // vermeld aantal pogingen voor yahtzee
+            Console.WriteLine("Aantal pogingen nodig: {0}", aantalPogingen);
+            // wacht op gebruiker
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OO Week 1/Yahtzee/YahtzeeGame.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yahtzee
+{
+    public class YahtzeeGame
+    {
+        public int[] dobbelstenen;
+        public Random rnd;
+
+        public YahtzeeGame()
+        {
+            int[] dobbelstenen = new int[5];
+
+            this.dobbelstenen = dobbelstenen;
+            this.rnd = new Random();
+        }
+
+        public void Gooi()
+        {           
+
+            for(int i = 0; i < 5; i++)
+            {
+               
+                dobbelstenen[i] = rnd.Next(1,6);
+                Console.WriteLine(dobbelstenen[i]);
+            }
+            
+
+        }
+        public bool Yahtzee()
+        {
+           int checkYahtzee = dobbelstenen[1];
+           
+            for (int i = 1; i < 5; i++)
+            {
+                if(dobbelstenen[i] == checkYahtzee)
+                {
+                    return true;
+                }
+            
+            }
+
+           
+        }
+
+    }
+}
