51e08fa2d92238287ab9f569f0c9966a221597af
royfi <royfillekes@hotmail.com>
2016-12-05 22:51:13 +0100

    UPdate

---
+++ OO week 2/Boekhandel/Cd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boekhandel
+{
+   public class Cd : Boek
+    {
+        string zanger;
+        public List<string>nummers;
+            
+        public Cd(string titel, string zanger, int exemplaren, double prijs, List<string> nummers)
+            :base (titel,exemplaren,prijs)
+        {
+            this.nummers = nummers;
+        }
+        public override void Print()
+        {
+            Console.WriteLine(" {0} \n {1} prijs : {2} \n Aantal Exemplaren : {3} \n nummers:  {4} \n", titel, zanger, prijs, exemplaren, nummers);
+        }
+
+    }
+}
--- OO week 2/Boekhandel/Program.cs
+++ OO week 2/Boekhandel/Program.cs
@@ -12,15 +12,28 @@ namespace Boekhandel
         {
             List<Boek> bladen = new List<Boek>();
             Boek boek = new Boek("Harry Potter", 10, 15.95);
-          //  Boekhandel.VoegToe(boek, bladen); Hier gaat iets fout
-          //  Boekhandel.PrintVoorraad(bladen);
+            //  Boekhandel.VoegToe(boek, bladen); Hier gaat iets fout
+            //  Boekhandel.PrintVoorraad(bladen);
             boek.WijzigVoorraad(45);
             DateTime datum = new DateTime(2016, 12, 05);
-            Weekblad weekblad = new Weekblad("Donald Duck", 33, 4.95, datum );
+            Weekblad weekblad = new Weekblad("Donald Duck", 33, 4.95, datum);
+
+            List<string> nummers = Nummers();
+            Cd cd = new Cd("Back Through Time", "Alestorm", 15, 18.75, nummers);
             weekblad.WijzigVoorraad(-5);
             boek.Print();
             weekblad.Print();
+            cd.Print();
             Console.ReadKey();
+        }
+
+        private static List<string> Nummers()
+        {
+            List<string> nummers = new List<string>();
+            nummers.Add("Shipwrecked");
+            nummers.Add("Keelhauled");
+            nummers.Add("Back Through Time");
+            return nummers;
         }
     }
 }
